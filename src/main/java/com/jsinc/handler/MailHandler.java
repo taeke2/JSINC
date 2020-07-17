e86a4ac289bfe7c9adb7b35a63d8b9e4b3bdf5a6
Heo seongtaek <taeke2@naver.com>
2020-07-17 15:00:11 +0900

    Handler Comment

--- src/main/java/com/jsinc/handler/MailHandler.java
+++ src/main/java/com/jsinc/handler/MailHandler.java
@@ -8,33 +8,41 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+// 작성자 : 서해준
+
 public class MailHandler {
 	private JavaMailSender sender;
 	private MimeMessage message;
 	private MimeMessageHelper messageHelper;
-
+	
+	// 메인 핸들러
 	public MailHandler(JavaMailSender sender) throws MessagingException {
 		this.sender = sender;
 		this.message = this.sender.createMimeMessage();
 		this.messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 	}
-
+	
+	// 보내는 메일 설정
 	public void setFrom(String mail, String name) throws UnsupportedEncodingException, MessagingException {
 		messageHelper.setFrom(mail, name);
 	}
 
+	// 받는 메일 설정
 	public void setTo(String mail) throws MessagingException {
 		messageHelper.setTo(mail);
 	}
-
+	
+	// 메일 제목 설정
 	public void setSubject(String subject) throws MessagingException {
 		messageHelper.setSubject(subject);
 	}
-
+	
+	// 메일 내용 설정
 	public void setText(String text) throws MessagingException {
 		messageHelper.setText(text, true);
 	}
-
+	
+	// 메일 보내기
 	public void send() {
 		try {
 			sender.send(message);
